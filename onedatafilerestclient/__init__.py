dd82f3449a838f90c7f3f00088f8caf23f10e46a
Bartek Kryza <bkryza@gmail.com>
2023-09-12 18:29:35 +0200

    Fixed export errors

--- onedatafilerestclient/__init__.py
+++ onedatafilerestclient/__init__.py
@@ -5,6 +5,7 @@ __author__ = "Bartek Kryza"
 __copyright__ = "Copyright (C) 2023 Onedata"
 __license__ = "This software is released under the MIT license cited in " \
               "LICENSE.txt"
+__all__ = ['OnedataRESTError', 'OnedataFileRESTClient']
 
 from .errors import OnedataRESTError  # noqa
 from .main import OnedataFileRESTClient  # noqa
