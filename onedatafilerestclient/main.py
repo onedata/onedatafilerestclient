774be7dee9fd86937883d9dc5475008a432edc61
Bartosz Walkowicz <wallko1@hotmail.com>
2024-05-08 10:43:34 +0200

    VFS-11937 Use ProviderSelector and OnezoneRESTClient in main.py

--- onedatafilerestclient/main.py
+++ onedatafilerestclient/main.py
@@ -10,29 +10,16 @@ __license__ = (
     "This software is released under the MIT license cited in LICENSE.txt")
 
 import json
-import random
-import time
 import typing
-from functools import lru_cache, wraps
-from typing import Any, Callable, Dict, Final, Iterator, NamedTuple, Optional
+from functools import wraps
+from typing import Any, Callable, Dict, Iterator, List, Optional
 
 import requests
 
-import semver  # type: ignore
-
 from . import OnedataRESTError
-from .access_token_scope import AccessTokenScope, SpaceDetails
 from .httpclient import HttpClient
-
-CACHE_SIZE_LIMIT: Final[int] = 512
-BLACKLIST_TIME_LIMIT_NS: Final[int] = 5 * 10**9
-
-
-class Provider(NamedTuple):
-    """Provider relevant attributes."""
-    id: str
-    version: semver.Version
-    domain: str
+from .onezone_rest_client import AccessTokenScope, OnezoneRESTClient
+from .provider_selector import ProviderSelector
 
 
 def _retry_on_provider_connection_error(
@@ -40,12 +27,13 @@ def _retry_on_provider_connection_error(
     @wraps(func)
     def wrapper(self: OnedataFileRESTClient, space_name: str, *args: Any,
                 **kwargs: Any) -> Any:
-        for provider in self._iter_available_space_providers(space_name):
+        for provider in self._provider_selector.iter_available_space_providers(
+                space_name, oz_rest_client=self._oz_client):
             try:
                 return func(self, space_name, *args, **kwargs)
             # TODO catch only connection exceptions
             except ValueError:
-                self._blaklist_provider(provider.id)
+                self._provider_selector.blacklist(provider.id)
 
         raise OnedataRESTError(400, 'posix',
                                f'No available providers for space {space_name}',
@@ -56,12 +44,13 @@ def _retry_on_provider_connection_error(
 
 class OnedataFileRESTClient:
     """Custom REST client for Onedata REST basic file operations API."""
+
+    # TODO property?
     onezone_host: str
     token: str
     preferred_oneproviders: list[str]
 
     client: HttpClient
-    token_client: HttpClient
 
     def __init__(self,
                  onezone_host: str,
@@ -70,86 +59,47 @@ class OnedataFileRESTClient:
                  *,
                  verify_ssl: bool = True):
         """Construct OnedataFileClient instance."""
-        super().__setattr__('onezone_host', onezone_host)
-        super().__setattr__('token', token)
+        self._oz_client = OnezoneRESTClient(host=onezone_host,
+                                            token=token,
+                                            verify_ssl=verify_ssl)
+        self._provider_selector = ProviderSelector(
+            preferred_provider_domains=preferred_oneproviders)
+
         self.client = HttpClient(verify_ssl=verify_ssl)
-        self.token_client = HttpClient(verify_ssl=verify_ssl)
-        self.preferred_oneproviders = preferred_oneproviders
-
-        # lru_cache cannot be used as decorator, as we want to have a separate
-        # cache for each OnedataFileRESTClient instance
-        self.get_space_id = lru_cache(maxsize=512)(self.get_space_id)
-
         self.client.get_session().headers.update({'X-Auth-Token': self.token})
-
-        self._init_cache()
-
-    def __setattr__(self, name: str, value: str) -> None:
-        """Dissalow modification of selected attributes due to caching."""
-        if name in ['token', 'onezone_host']:
-            raise AttributeError(
-                f'"{name}" attribute cannot be changed after construction.')
-
-        super().__setattr__(name, value)
 
     def __eq__(self, other: object) -> bool:
         """Compare 2 instances of OnedataFileClient."""
         if not isinstance(other, OnedataFileRESTClient):
             return NotImplemented
-        return (self.onezone_host == other.onezone_host) \
-            and (self.token == other.token)
+
+        return self._oz_client == other._oz_client
 
     def __hash__(self) -> int:
         """Calculate a hash of a given instance of OnedataFileClient."""
-        return hash(self.onezone_host) ^ hash(self.token)
+        return hash(self._oz_client)
 
     def oz_url(self, path: str) -> str:
         """Generate Onezone URL for specific path."""
-        return f'https://{self.onezone_host}/api/v3/onezone{path}'
+        return self._oz_client.build_url(path)
 
     def get_token_scope(self) -> AccessTokenScope:
         """Get current token access scope."""
-        url = self.oz_url('/tokens/infer_access_token_scope')
-        caps = self.token_client.post(url, {'token': self.token})
-        return typing.cast(AccessTokenScope, caps.json())
+        return self._oz_client.infer_token_scope()
 
-    def list_spaces(self) -> list[str]:
+    def list_spaces(self) -> List[str]:
         """List all spaces available for the current token."""
-        access_token_scope = self.get_token_scope()
-
-        def is_space_supported(s: SpaceDetails) -> bool:
-            return 'supports' in s and bool(s['supports'])
-
-        all_spaces = access_token_scope['dataAccessScope']['spaces']
-
-        # TODO space_name@space_id ?
-        supported_spaces = [
-            space_details['name'] for space_details in all_spaces.values()
-            if is_space_supported(space_details)
-        ]
-
-        return supported_spaces
+        return self._oz_client.list_spaces()
 
     def get_space_id(self, space_name: str) -> str:
         """Get space id by name."""
-        return self._get_space_id(space_name, self.get_token_scope())
-
-    # TODO handle space_name@space_id
-    def _get_space_id(self, space_name: str,
-                      access_token_scope: AccessTokenScope) -> str:
-        """Get space id by name."""
-        spaces = access_token_scope['dataAccessScope']['spaces']
-
-        for space_id, space_details in spaces.items():
-            if space_details['name'] == space_name:
-                space_id
-
-        raise OnedataRESTError(400, 'posix',
-                               f'Space {space_name} doesn\'t exist', 'enoent')
+        return self._oz_client.get_space_id(space_name)
 
     def get_provider_for_space(self, space_name: str) -> str:
         """Get Oneprovider domain for a specific space."""
-        provider = next(self._iter_available_space_providers(space_name))
+        provider = next(
+            self._provider_selector.iter_available_space_providers(
+                space_name, oz_rest_client=self._oz_client))
         return provider.domain
 
     def op_url(self, space_name: str, path: str) -> str:
@@ -299,86 +249,9 @@ class OnedataFileRESTClient:
             "Content-type": "application/cdmi-object"
         }
 
-        provider = self._provider_for_space[src_space_name]
-        url = f'https://{provider.domain}/cdmi/{dst_space_name}/{dst_file_path}'
+        provider_domain = self.get_provider_for_space(src_space_name)
+        url = f'https://{provider_domain}/cdmi/{dst_space_name}/{dst_file_path}'
 
         data = {'move': f'{src_space_name}/{src_file_path}'}
 
         self.client.put(url, data=json.dumps(data), headers=headers)
-
-    def _init_cache(self) -> None:
-        self._provider_blacklist: Dict[str, int] = {}
-        self._provider_for_space: Dict[str, Provider] = {}
-
-    def _is_provider_blacklisted(self, provider_id: str) -> bool:
-        if provider_id in self._provider_blacklist:
-            blacklist_time_end = self._provider_blacklist[provider_id]
-            if blacklist_time_end > time.time_ns():
-                True
-            else:
-                del self._provider_blacklist[provider_id]
-                return False
-
-        return False
-
-    def _blaklist_provider(self, provider_id: str) -> None:
-        blacklist_time_end = time.time_ns() + BLACKLIST_TIME_LIMIT_NS
-
-        if len(self._provider_blacklist) >= CACHE_SIZE_LIMIT:
-            self._provider_blacklist = {provider_id: blacklist_time_end}
-        else:
-            self._provider_blacklist[provider_id] = blacklist_time_end
-
-    def _iter_available_space_providers(self,
-                                        space_name: str) -> Iterator[Provider]:
-        if space_name in self._provider_for_space:
-            yield self._provider_for_space[space_name]
-        elif len(self._provider_for_space) >= CACHE_SIZE_LIMIT:
-            # clear cache
-            self._provider_for_space = {}
-
-        for provider in self._list_available_space_providers(space_name):
-            self._provider_for_space[space_name] = provider
-            yield provider
-
-    def _list_available_space_providers(self,
-                                        space_name: str) -> list[Provider]:
-        access_token_scope = self.get_token_scope()
-
-        all_providers = access_token_scope['dataAccessScope']['providers']
-
-        space_id = self._get_space_id(space_name, access_token_scope)
-        space_details = access_token_scope['dataAccessScope']['spaces'][
-            space_id]
-
-        preferred_supporting_providers = []
-        remaining_supporting_providers = []
-
-        for provider_id in space_details['supports']:
-            if self._is_provider_blacklisted(provider_id):
-                continue
-
-            provider_details = all_providers[provider_id]
-            if not provider_details['online']:
-                continue
-
-            provider = Provider(id=provider_id,
-                                version=semver.Version.parse(
-                                    provider_details['version']),
-                                domain=provider_details['domain'])
-
-            try:
-                index = self.preferred_oneproviders.index(provider.domain)
-                preferred_supporting_providers.append((index, provider))
-            except ValueError:
-                remaining_supporting_providers.append(provider)
-
-        preferred_supporting_providers.sort()
-        random.shuffle(remaining_supporting_providers)
-
-        supporting_providers = [
-            provider for _, provider in preferred_supporting_providers
-        ]
-        supporting_providers.extend(remaining_supporting_providers)
-
-        return supporting_providers
