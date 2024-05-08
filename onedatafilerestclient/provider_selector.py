dced89eba53d026562916158886695ee5a2dda99
Bartosz Walkowicz <wallko1@hotmail.com>
2024-05-08 10:41:27 +0200

    VFS-11937 Add provider_selector.py

---
+++ onedatafilerestclient/provider_selector.py
@@ -0,0 +1,123 @@
+"""Provider selector utilities."""
+
+__author__ = "Bartosz Walkowicz"
+__copyright__ = "Copyright (C) 2024 ACK CYFRONET AGH"
+__license__ = (
+    "This software is released under the MIT license cited in LICENSE.txt")
+
+import random
+import time
+from typing import Dict, Final, Iterator, List, NamedTuple, Optional
+
+import semver  # type: ignore
+
+from .onezone_rest_client import OnezoneRESTClient, ProviderId
+
+_CACHE_SIZE_LIMIT: Final[int] = 512
+_BLACKLIST_TIME_LIMIT_NS: Final[int] = 5 * 10**9
+
+
+class Provider(NamedTuple):
+    """Provider relevant attributes."""
+    id: str
+    version: semver.Version
+    domain: str
+
+
+class ProviderSelector:
+    """Selector responsible for choosing available provider(s) for space."""
+
+    preferred_provider_domains: List[str]
+    _provider_blacklist: Dict[str, int]
+    _provider_for_space: Dict[str, Provider]
+
+    def __init__(
+            self,
+            *,
+            preferred_provider_domains: Optional[List[str]] = None) -> None:
+        """Construct ProviderSelector instance."""
+        self.preferred_provider_domains = preferred_provider_domains or []
+        self._provider_blacklist = {}
+        self._provider_for_space = {}
+
+    def is_blacklisted(self, provider_id: ProviderId) -> bool:
+        """Blacklist specified provider for a short while."""
+        if provider_id not in self._provider_blacklist:
+            return False
+
+        blacklist_time_end = self._provider_blacklist[provider_id]
+        if blacklist_time_end > time.time_ns():
+            return True
+        else:
+            del self._provider_blacklist[provider_id]
+            return False
+
+    def blacklist(self, provider_id: ProviderId) -> None:
+        """Check if specified provider is blacklisted."""
+        blacklist_time_end = time.time_ns() + _BLACKLIST_TIME_LIMIT_NS
+
+        if len(self._provider_blacklist) > _CACHE_SIZE_LIMIT:
+            self._provider_blacklist = {provider_id: blacklist_time_end}
+        else:
+            self._provider_blacklist[provider_id] = blacklist_time_end
+
+    def iter_available_space_providers(
+            self, space_name: str, *,
+            oz_rest_client: OnezoneRESTClient) -> Iterator[Provider]:
+        """Iterate over online and not not blacklisted space providers."""
+        if space_name in self._provider_for_space:
+            yield self._provider_for_space[space_name]
+
+        if len(self._provider_for_space) > _CACHE_SIZE_LIMIT:
+            # clear cache
+            self._provider_for_space = {}
+
+        for provider in self.list_available_space_providers(
+                space_name, oz_rest_client=oz_rest_client):
+            self._provider_for_space[space_name] = provider
+            yield provider
+
+    def list_available_space_providers(
+            self, space_name: str, *,
+            oz_rest_client: OnezoneRESTClient) -> List[Provider]:
+        """List online and not not blacklisted space providers."""
+        access_token_scope = oz_rest_client.infer_token_scope()
+
+        all_providers = access_token_scope['dataAccessScope']['providers']
+
+        space_id = oz_rest_client.get_space_id(
+            space_name, access_token_scope=access_token_scope)
+        space_details = access_token_scope['dataAccessScope']['spaces'][
+            space_id]
+
+        preferred_supporting_providers = []
+        remaining_supporting_providers = []
+
+        for provider_id in space_details['supports']:
+            if self.is_blacklisted(provider_id):
+                continue
+
+            provider_details = all_providers[provider_id]
+            if not provider_details['online']:
+                continue
+
+            provider_version = semver.Version.parse(provider_details['version'])
+            provider = Provider(id=provider_id,
+                                version=provider_version,
+                                domain=provider_details['domain'])
+
+            try:
+                index = self.preferred_provider_domains.index(provider.domain)
+                preferred_supporting_providers.append((index, provider))
+            except ValueError:
+                remaining_supporting_providers.append(provider)
+
+        preferred_supporting_providers.sort()
+        supporting_providers = [
+            provider for _, provider in preferred_supporting_providers
+        ]
+
+        random.shuffle(remaining_supporting_providers)
+        supporting_providers.extend(remaining_supporting_providers)
+
+        return supporting_providers
