61d46b3100eec451d5f04974683d36ae71e24638
Bartosz Walkowicz <wallko1@hotmail.com>
2024-05-27 14:37:27 +0200

    VFS-11937 Log all http requests results

--- onedatafilerestclient/http_client.py
+++ onedatafilerestclient/http_client.py
@@ -14,6 +14,10 @@ from requests.structures import CaseInsensitiveDict
 
 from .errors import OnedataRESTError
 
+_logger = logging.getLogger(__name__)
+
+_HEADERS_TO_LOG = {"range"}
+
 
 class HttpClient:
     """REST-style wrapper over requests library."""
@@ -29,35 +33,6 @@ class HttpClient:
     def get_session(self) -> requests.Session:
         """Return requests session instance."""
         return self.session
-
-    def _send_request(
-        self,
-        method: str,
-        url: str,
-        data: Any = None,
-        headers: Optional[Dict[str, str]] = None,
-        *,
-        stream: bool = False,
-    ) -> requests.Response:
-        """Perform an HTTP request."""
-        if isinstance(data, dict):
-            body = json.dumps(data)
-        else:
-            body = data
-
-        case_insensitive_headers = CaseInsensitiveDict(headers)
-        if "content-type" not in case_insensitive_headers:
-            case_insensitive_headers["content-type"] = "application/json"
-
-        req = requests.Request(method, url, data=body, headers=case_insensitive_headers)
-        prepared = self.session.prepare_request(req)
-        response = self.session.send(prepared, stream=stream, timeout=self.timeout)
-
-        if not response.ok:
-            logging.debug("ERROR: %s %s '%s'", method, url, response.text)
-            raise OnedataRESTError.from_response(response)
-
-        return response
 
     def get(
         self,
@@ -93,3 +68,49 @@ class HttpClient:
     ) -> requests.Response:
         """Perform a HEAD request."""
         return self._send_request("HEAD", url, data, headers)
+
+    def _send_request(
+        self,
+        method: str,
+        url: str,
+        data: Any = None,
+        headers: Optional[Dict[str, str]] = None,
+        *,
+        stream: bool = False,
+    ) -> requests.Response:
+        """Perform an HTTP request."""
+        if isinstance(data, dict):
+            body = json.dumps(data)
+        else:
+            body = data
+
+        case_insensitive_headers = CaseInsensitiveDict(headers)
+        if "content-type" not in case_insensitive_headers:
+            case_insensitive_headers["content-type"] = "application/json"
+
+        req = requests.Request(method, url, data=body, headers=case_insensitive_headers)
+        prepared = self.session.prepare_request(req)
+        response = self.session.send(prepared, stream=stream, timeout=self.timeout)
+
+        if not response.ok:
+            ex = OnedataRESTError.from_response(response)
+            if response.status_code == 403:
+                _logger.debug(
+                    "AUTHORIZATION FAILED %s %s\nheaders: %s\nbody: %s\ndetails: %s",
+                    method,
+                    url,
+                    {
+                        k: v
+                        for k, v in case_insensitive_headers.items()
+                        if k.lower() in _HEADERS_TO_LOG
+                    },
+                    data if isinstance(data, dict) else len(data),
+                    ex,
+                )
+            else:
+                _logger.debug("REQUEST FAILED %s %s\ndetails: %s", method, url, ex)
+
+            raise ex
+
+        _logger.debug("REQUEST OK %d %s %s", response.status_code, method, url)
+        return response
