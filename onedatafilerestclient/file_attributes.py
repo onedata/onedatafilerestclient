60820d593bf7c4aa3d18fa569e5aa10c78561209
Bartosz Walkowicz <wallko1@hotmail.com>
2024-06-26 10:15:15 +0200

    VFS-12045 Add creationTime file attribute

--- onedatafilerestclient/file_attributes.py
+++ onedatafilerestclient/file_attributes.py
@@ -23,7 +23,8 @@ else:
     from typing import TypeAlias, TypedDict
 
 
-_PROVIDER_SUPPORTING_CURRENT_API_KEY_MIN_VERSION: Final[Version] = Version("21.2.5")
+_MIN_SUPPORTED_PROVIDER_API_VERSION: Final[Version] = Version("21.2.0")
+_PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION: Final[Version] = Version("21.2.5")
 
 _NOT_SUPPORTED_ATTR_ERROR_DETAILS_FMT: Final[str] = (
     "The provider chosen for this space ({domain}) is in version ({version}) "
@@ -44,6 +45,7 @@ BasicFileAttrKey: TypeAlias = Literal[
     "parentFileId",
     "displayGid",
     "displayUid",
+    "creationTime",
     "atime",
     "mtime",
     "ctime",
@@ -64,6 +66,42 @@ BasicFileAttrKey: TypeAlias = Literal[
     "archiveRecallRootFileId",
 ]
 
+_MIN_PROVIDER_VERSION_SUPPORTING_BASIC_ATTR_KEY: Final[
+    Dict[BasicFileAttrKey, Version]
+] = {
+    "fileId": _MIN_SUPPORTED_PROVIDER_API_VERSION,
+    "index": _MIN_SUPPORTED_PROVIDER_API_VERSION,
+    "type": _MIN_SUPPORTED_PROVIDER_API_VERSION,
+    "activePermissionsType": _PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION,
+    "posixPermissions": _MIN_SUPPORTED_PROVIDER_API_VERSION,
+    "acl": _PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION,
+    "name": _MIN_SUPPORTED_PROVIDER_API_VERSION,
+    "conflictingName": _PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION,
+    "path": _PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION,
+    "parentFileId": _MIN_SUPPORTED_PROVIDER_API_VERSION,
+    "displayGid": _MIN_SUPPORTED_PROVIDER_API_VERSION,
+    "displayUid": _MIN_SUPPORTED_PROVIDER_API_VERSION,
+    "creationTime": Version("21.2.6"),
+    "atime": _MIN_SUPPORTED_PROVIDER_API_VERSION,
+    "mtime": _MIN_SUPPORTED_PROVIDER_API_VERSION,
+    "ctime": _MIN_SUPPORTED_PROVIDER_API_VERSION,
+    "size": _MIN_SUPPORTED_PROVIDER_API_VERSION,
+    "isFullyReplicatedLocally": _PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION,
+    "localReplicationRate": _PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION,
+    "originProviderId": _MIN_SUPPORTED_PROVIDER_API_VERSION,
+    "directShareIds": _MIN_SUPPORTED_PROVIDER_API_VERSION,
+    "ownerUserId": _MIN_SUPPORTED_PROVIDER_API_VERSION,
+    "hardlinkCount": _MIN_SUPPORTED_PROVIDER_API_VERSION,
+    "symlinkValue": _PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION,
+    "hasCustomMetadata": _PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION,
+    "effProtectionFlags": _PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION,
+    "effDatasetProtectionFlags": _PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION,
+    "effDatasetInheritancePath": _PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION,
+    "effQosInheritancePath": _PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION,
+    "aggregateQosStatus": _PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION,
+    "archiveRecallRootFileId": _PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION,
+}
+
 XattrKey: TypeAlias = str
 """
 File extended attribute in the form xattr.<NAME>
@@ -71,7 +109,7 @@ File extended attribute in the form xattr.<NAME>
 
 FileAttrKey = Union[BasicFileAttrKey, XattrKey]
 
-_DEPRECATED_BASIC_FILE_ATTR_KEYS: Dict[FileAttrKey, str] = {
+_SNAKE_CASE_BASIC_FILE_ATTR_KEYS: Final[Dict[BasicFileAttrKey, str]] = {
     "fileId": "file_id",
     "index": "index",
     "type": "type",
@@ -139,6 +177,7 @@ class FileAttrsJson(TypedDict, total=False):
     parentFileId: Optional[str]
     displayGid: int
     displayUid: int
+    creationTime: int
     atime: int
     mtime: int
     ctime: int
@@ -171,19 +210,17 @@ def build_http_get_file_attr_params(
     body = None
 
     if requested_attr_keys is not None:
-        if provider.version < _PROVIDER_SUPPORTING_CURRENT_API_KEY_MIN_VERSION:
+        _assert_attr_keys_supported(provider, requested_attr_keys)
+
+        if provider.version < _PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION:
             # Due to a bug it is not possible to fetch more than one specific
             # attribute for file using old API. Trying to do so results in empty
             # json returned. As a workaround all attributes are fetched (no qs)
             if len(requested_attr_keys) <= 1:
                 qs = "&".join(
-                    f"attribute={_get_deprecated_api_attr_key(provider, attr_key)}"
+                    f"attribute={_get_snake_case_attr_key(attr_key)}"
                     for attr_key in requested_attr_keys
                 )
-            else:
-                # ensure all requested attributes are supported
-                for attr_key in requested_attr_keys:
-                    _get_deprecated_api_attr_key(provider, attr_key)
         else:
             body = {"attributes": requested_attr_keys}
 
@@ -202,9 +239,11 @@ def build_http_list_children_attrs_params(
     body = None
 
     if requested_attr_keys is not None:
-        if provider.version < _PROVIDER_SUPPORTING_CURRENT_API_KEY_MIN_VERSION:
+        _assert_attr_keys_supported(provider, requested_attr_keys)
+
+        if provider.version < _PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION:
             qs = "&".join(
-                f"attribute={_get_deprecated_api_attr_key(provider, attr_key)}"
+                f"attribute={_get_snake_case_attr_key(attr_key)}"
                 for attr_key in requested_attr_keys
             )
         else:
@@ -213,24 +252,35 @@ def build_http_list_children_attrs_params(
     return qs, body
 
 
-def _get_deprecated_api_attr_key(
-    provider: SpaceSupportingProvider, attr_key: FileAttrKey
-) -> str:
-    deprecated_attr_key = _DEPRECATED_BASIC_FILE_ATTR_KEYS.get(attr_key)
-    if deprecated_attr_key is not None:
-        return deprecated_attr_key
+def _assert_attr_keys_supported(
+    provider: SpaceSupportingProvider, attr_keys: List[FileAttrKey]
+) -> None:
+    for attr_key in attr_keys:
+        _assert_attr_key_supported(provider, attr_key)
 
-    raise OnedataRESTError(
-        http_code=400,
-        category="posix",
-        description="einval",
-        details=_NOT_SUPPORTED_ATTR_ERROR_DETAILS_FMT.format(
-            domain=provider.domain,
-            version=provider.version,
-            attr=attr_key,
-            min_required_version=_PROVIDER_SUPPORTING_CURRENT_API_KEY_MIN_VERSION,
-        ),
-    )
+
+def _assert_attr_key_supported(
+    provider: SpaceSupportingProvider, attr_key: FileAttrKey
+) -> None:
+    if attr_key.startswith("xattr."):
+        min_required_version = _PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION
+    else:
+        min_required_version = _MIN_PROVIDER_VERSION_SUPPORTING_BASIC_ATTR_KEY[
+            cast(BasicFileAttrKey, attr_key)
+        ]
+
+    if provider.version < min_required_version:
+        raise OnedataRESTError(
+            http_code=400,
+            category="posix",
+            description="einval",
+            details=_NOT_SUPPORTED_ATTR_ERROR_DETAILS_FMT.format(
+                attr=attr_key,
+                domain=provider.domain,
+                version=provider.version,
+                min_required_version=min_required_version,
+            ),
+        )
 
 
 def normalize_file_attrs_json(
@@ -243,9 +293,9 @@ def normalize_file_attrs_json(
         # default api attrs
         requested_attr_keys = _DEFAULT_API_FILE_ATTR_KEYS
 
-    if provider.version < _PROVIDER_SUPPORTING_CURRENT_API_KEY_MIN_VERSION:
+    if provider.version < _PROVIDER_SUPPORTING_CAMEL_CASE_API_KEYS_MIN_VERSION:
         sanitized_file_attrs_json = {
-            attr_key: file_attrs_json[_DEPRECATED_BASIC_FILE_ATTR_KEYS[attr_key]]
+            attr_key: file_attrs_json[_get_snake_case_attr_key(attr_key)]
             for attr_key in requested_attr_keys
         }
     else:
@@ -254,3 +304,7 @@ def normalize_file_attrs_json(
         }
 
     return cast(FileAttrsJson, sanitized_file_attrs_json)
+
+
+def _get_snake_case_attr_key(attr_key: FileAttrKey) -> str:
+    return _SNAKE_CASE_BASIC_FILE_ATTR_KEYS[cast(BasicFileAttrKey, attr_key)]
