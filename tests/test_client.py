afaab0221460ccaeaff590e59fa66c182d04d136
Bartosz Walkowicz <16596719+bwalkowi@users.noreply.github.com>
2025-11-24 18:02:33 +0100

    VFS-13149 Add test_provider_selector_blacklist_isolation

--- tests/test_client.py
+++ tests/test_client.py
@@ -182,7 +182,7 @@ def test_provider_selector(
     )
 
     # pylint: disable=W0212
-    client._provider_selector._blacklist_time_limit_ns = 10 * 10**9
+    client._provider_selector._blacklist_time_limit_ns = 1 * 10**9
 
     space_specifier = _random_space_specifier(SPACE_KRK_PAR_NAME, client)
     file_id = _create_and_sync_file_in_space(
@@ -223,8 +223,23 @@ def test_provider_selector(
 
         assert exc_info.value.args == (_ensure_fqn(space_specifier, client),)
 
+    # at this point 'first_choice_provider' should be blacklisted, but because
+    # there are no other providers available, it should be returned as fallback
+    # pylint: disable=W0212
+    provider_selector = client._provider_selector
+    first_provider_id = _get_provider_id(first_choice_provider)
+    space_id = client.get_space_id(space_specifier)
+    assert provider_selector.is_blacklisted(first_provider_id, space_id)
+
     # but on the next try, again the blacklisted provider should be selected
     # (since blacklist time has not yet expired and it is the fallback)
+    assert_selected_provider(first_choice_provider)
+
+    # after blacklist time ends 'first_choice_provider' should be
+    # again selected as the primary provider (not just fallback)
+    # we verify this by ensuring it is selected even if we don't force fallback
+    time.sleep(2)
+    assert not provider_selector.is_blacklisted(first_provider_id, space_id)
     assert_selected_provider(first_choice_provider)
 
 
@@ -300,6 +315,11 @@ def test_provider_selector_with_readonly_provider(
     # should be returned as fallback
     assert_selected_provider(first_choice_provider, second_choice_provider)
 
+    # check that 'first_choice_provider' is indeed blacklisted
+    # pylint: disable=W0212
+    provider_selector = client._provider_selector
+    assert provider_selector.is_blacklisted(first_choice_provider_id, space_id)
+
     # with connection error raised by 'second_choice_provider' there should
     # be no available providers for write (first is readonly fallback),
     # but read should still work using fallback
@@ -311,6 +331,12 @@ def test_provider_selector_with_readonly_provider(
 
     client.get_attributes(space_specifier, file_id=file_id)
     assert get_selected_provider_domain(False) == first_choice_provider
+
+    # after blacklist time ends 'first_choice_provider' should be
+    # again selected as the primary provider for read and write
+    time.sleep(2)
+    assert not provider_selector.is_blacklisted(first_choice_provider_id, space_id)
+    assert_selected_provider(first_choice_provider, second_choice_provider)
 
 
 def test_provider_selector_with_offline_provider(onezone_ip, onezone_admin_token):
@@ -361,6 +387,40 @@ def test_provider_selector_with_dummy_provider(onezone_ip, onezone_admin_token):
     assert (
         _get_selected_provider(client_2, space_specifier).domain == PROVIDER_PAR_DOMAIN
     )
+
+
+def test_provider_selector_blacklist_isolation(onezone_ip, onezone_admin_token):
+    """Test that blacklisting a provider in one space does not affect other spaces."""
+    provider_krk = PROVIDER_KRK_DOMAIN
+    provider_par = PROVIDER_PAR_DOMAIN
+    provider_par_id = _get_provider_id(provider_par)
+
+    client = OnedataFileRESTClient(
+        onezone_ip,
+        onezone_admin_token,
+        [provider_par],
+        verify_ssl=False,
+    )
+    # pylint: disable=W0212
+    client._provider_selector._blacklist_time_limit_ns = 10 * 10**9
+
+    space_krk_par_id = client.get_space_id(SPACE_KRK_PAR_NAME)
+    space_par_id = client.get_space_id(SPACE_PAR_NAME)
+
+    # Verify Paris is selected for Space Krk-Par
+    assert _get_selected_provider(client, SPACE_KRK_PAR_NAME).domain == provider_par
+
+    # Cause connection error on Space Krk-Par (Paris)
+    with _mock_http_client([provider_par]):
+        client.get_attributes(SPACE_KRK_PAR_NAME)
+
+    # Verify Paris is blacklisted for Space Krk-Par
+    assert client._provider_selector.is_blacklisted(provider_par_id, space_krk_par_id)
+    assert _get_selected_provider(client, SPACE_KRK_PAR_NAME).domain == provider_krk
+
+    # Verify we can still use Paris for Space Par
+    assert not client._provider_selector.is_blacklisted(provider_par_id, space_par_id)
+    assert _get_selected_provider(client, SPACE_PAR_NAME).domain == provider_par
 
 
 def test_get_file_id(client: OnedataFileRESTClient):
